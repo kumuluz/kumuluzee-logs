7ad48459d8d166326c105793e1c96077a793744e
Rok <povse.rok@gmail.com>
2017-03-17 16:51:41 +0100

    Added Log4j2LogUtil class for transformation from/to Log4j2 levels

---
+++ log4j2/src/main/java/com/kumuluz/ee/logs/utils/Log4j2LogUtil.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Sunesis d.o.o.
+ */
+package com.kumuluz.ee.logs.utils;
+
+import com.kumuluz.ee.logs.enums.LogLevel;
+import org.apache.logging.log4j.Level;
+
+/**
+ * Kumuluz-logs logger interface
+ *
+ * @author Rok Povse, Marko Skrjanec
+ */
+public class Log4j2LogUtil {
+    public static Level convertToLog4j2Level(LogLevel logLevel) {
+        return Level.getLevel(logLevel.toString());
+    }
+    public static LogLevel convertFromLog4j2Level(Level level) {
+        return LogLevel.valueOf(level.name());
+    }
+}
